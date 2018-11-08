522e61afeb6e6a4477dc3e21988034f09601c06b
Jerry Moua <jmoua4@uncc.edu>
2018-11-08 04:31:50 +0000

    added CRUD, Edit and View Functionality to Logs Controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,4 +1,44 @@
 class LogsController < ApplicationController
+    def index
+        @log = Log.all
+    end 
+    def show
+        @log = Log.find(params[:id])
+    end 
     def new
+        @log = Log.new
     end
+    def create
+        @log = Log.new(log_params)
+        byebug
+        
+        if @log.save
+            redirect_to @log
+            byebug
+        else
+            render 'new'
+        end
+    end
+    def edit
+        @log = Log.find(params[:id])
+    end 
+    def update
+        @log = Log.find(params[:id])
+        if @log.update(log_params)
+            redirect_to @log
+        else
+            render 'edit'
+        end
+    end 
+    def destroy
+        @log = Log.find(params[:id])
+        @log.destroy
+        
+        redirect_to logs_path
+    end 
 end
+
+private
+    def log_params
+        params.require(:log).permit(:title, :text)
+    end 
