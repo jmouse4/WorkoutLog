9292994d7c3eb02ae60a1a6665b007a990509c75
Zack <zpruitt3@uncc.edu>
2018-12-01 18:42:28 +0000

    Add rspec workout log scenario 2

--- spec/integration/trainee_adds_a_workout_spec.rb
+++ spec/integration/trainee_adds_a_workout_spec.rb
@@ -10,4 +10,14 @@ feature "Trainee adds a workout log" do
     expect(page).to have_field("Title")
     expect(page).to have_field("Text")
     end
+    
+    scenario "Trainee successfully adds new workout log" do
+    visit new_log_path
+    expect(page).to have_content("New Workout Log")
+    fill_in "Title", with: "New workout log"
+    fill_in "Text", with: "New workout log 1"
+    click_button "Create Log"
+    expect(page).to have_content("New workout log")
+    expect(page).to have_content("New workout log 1")
+    end
 end
