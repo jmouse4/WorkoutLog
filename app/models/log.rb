2341504ef5acfa47624a5c0923f55bf3071e215c
Jerry Moua <jmoua4@uncc.edu>
2018-12-01 05:58:06 +0000

    Changed and added links in Log index/show and validation limits

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,5 @@
 class Log < ApplicationRecord
     has_many :pictures
-    validates :title, presence: true, length: { minimum: 5 }
-    validates :text, presence: true, length: { minimum: 10}
+    validates :title, presence: true, length: { minimum: 4 }
+    validates :text, presence: true, length: { minimum: 6}
 end
