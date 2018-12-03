9c086d03e8d00a2da4c0d57a228c3b285afc59cb
Zack <zpruitt3@uncc.edu>
2018-12-03 22:49:31 +0000

    Added two more testing scenarios

--- spec/integration/trainee_adds_a_workout_spec.rb
+++ spec/integration/trainee_adds_a_workout_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper.rb'
 
-feature "Trainee adds a workout log" do
+feature "Trainee navigates to the workout log" do
     scenario "Trainee successfully navigates to the new workout log page from the homepage" do
     
     visit logs_path
@@ -19,5 +19,33 @@ feature "Trainee adds a workout log" do
     click_button "Create Log"
     expect(page).to have_content("New workout log")
     expect(page).to have_content("New workout log 1")
+    end
+    
+    scenario "Trainee successfully updates their workout log" do
+        
+    visit new_log_path
+    expect(page).to have_content("Workout Logs")
+    click_link "New Workout Log"
+    fill_in "Title", with: "New workout log"
+    fill_in "Text", with: "New workout log 1"
+    click_button "Create Log"
+    click_link "Edit"
+    expect(page).to have_content("New workout log")
+    fill_in "Title", with: "New workout log (edited"
+    fill_in "Text", with: "New workout log 1 (edited)"
+    click_button "Update Log"
+    expect(page).to have_content("New workout log 1 (edited)")
+    end
+    
+    scenario "Trainee successfully deletes their workout log" do
+    
+    visit new_log_path
+    expect(page).to have_content("Workout Logs")
+    click_link "New Workout Log"
+    fill_in "Title", with: "New workout log (destroy)"
+    fill_in "Text", with: "New workout log 1 (destroy)"
+    click_button "Create Log"
+    click_link "Destroy"
+    expect(page).to have_no_content("New workout log (destroy)")
     end
 end
